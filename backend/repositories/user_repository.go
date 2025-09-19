feea49fb7f630e802cd61a3c4b16fdd77268efe9
Mono <manubotta111@gmail.com>
2025-09-18 21:18:34 -0300

    repository getuser

--- backend/repositories/user_repository.go
+++ backend/repositories/user_repository.go
@@ -2,19 +2,35 @@ package repositories
 
 import (
     "context"
-	"Code-Laners\backend\models"
+	"Code-Laners/backend/models"
     "go.mongodb.org/mongo-driver/bson"
     "go.mongodb.org/mongo-driver/bson/primitive"
     "go.mongodb.org/mongo-driver/mongo"
 )
 
 type UserRepositoryInterface interface{
-	GetUser(nombre string) ([]model.Avion, error)
-    GetUserByID(id string) (model.Avion, error)
-    CreateUser(avion model.Avion) (*mongo.InsertOneResult, error)
-    UpdateUser(avion model.Avion) (*mongo.UpdateResult, error)
+	GetUser(name string) ([]models.User, error)
+    GetUserByID(id string) (models.User, error)
+    CreateUser(user models.User) (*mongo.InsertOneResult, error)
+    UpdateUser(user models.User) (*mongo.UpdateResult, error)
     DeleteUser(id primitive.ObjectID) (*mongo.DeleteResult, error)
 }
 type UserRepository struct{
 	db DB
+}
+func NewUserRepository(db DB) *UserRepository {
+    return &UserRepository{
+        db: db,
+    }
+}
+func (repository UserRepository) GetUser(name string)([]models.User, error){
+	collection := repository.db.GetClient().Database("fitness_db").collection("users")
+
+	var filter bson.M
+
+	if name != ""{
+		filter = bson.M{"name": bson.M{"$regex": name, "$options": "i"}}
+	} else {
+        filter = bson.M{}
+    }
 }
