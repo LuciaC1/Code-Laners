8978aa22660458541a689484cbfe360b9fbe65ac
Lucia <lucicopes@gmail.com>
2025-09-19 19:11:52 -0300

    comentarios main de ayuda

--- backend/main.go
+++ backend/main.go
@@ -1,1 +1,10 @@
 package main
+
+// main.go: Archivo donde solo se manejan las rutas
+// handlers: Funciones que atienden cada ruta.
+
+// services: Funciones de lógica de negocio.
+// dto: Archivos con struct de request, response
+
+// repositories: métodos de acceso a datos
+// model: estructuras para mapeos de ORM
