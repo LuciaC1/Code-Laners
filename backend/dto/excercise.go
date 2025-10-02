3db1391d7ee6e488ebe23a2b7b50ad1bae763025
Lucia <lucicopes@gmail.com>
2025-10-02 20:29:52 -0300

    exercise dto fix

--- backend/dto/excercise.go
+++ backend/dto/excercise.go
@@ -1,16 +1,14 @@
 package dto
 
-import "go.mongodb.org/mongo-driver/bson/primitive"
-
 type ExerciseDTO struct {
-	UserID      primitive.ObjectID `json:"user_id"`
-	Name        string             `json:"name" binding:"required"`
-	Description string             `json:"description,omitempty"`
-	Category    string             `json:"category" binding:"required"`
-	MuscleGroup string             `json:"muscle_group" binding:"required"`
-	Difficulty  string             `json:"difficulty" binding:"required"`
-	MediaURL    string             `json:"media_url,omitempty"`
-	Steps       []string           `json:"steps,omitempty"`
+	UserID      string   `json:"user_id"`
+	Name        string   `json:"name" binding:"required"`
+	Description string   `json:"description,omitempty"`
+	Category    string   `json:"category" binding:"required"`
+	MuscleGroup string   `json:"muscle_group" binding:"required"`
+	Difficulty  string   `json:"difficulty" binding:"required"`
+	MediaURL    string   `json:"media_url,omitempty"`
+	Steps       []string `json:"steps,omitempty"`
 }
 
 type ExerciseRequest struct {
